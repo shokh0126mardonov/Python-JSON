217d992eb6c7513d864e05ef03d4e067050dd940
shokh0126mardonov <smardonov203@gmail.com>
2025-07-03 16:36:30 +0500

    bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,13 @@
+import json
 
+with open('students.json','r') as jsonfile:
+    json_str = jsonfile.read()
+    students = json.loads(json_str)
+
+score_rating = list(sorted(students,key=lambda score:score['score'],reverse=True))
+
+json_score_rating = json.dumps(score_rating,indent = 4)
+
+with open('rating.json','w') as f2:
+    f2.write(json_score_rating)
+    
